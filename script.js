d61262e776caeb02734cf4d3cb7d7fe51c19a4dc
Nabeel Ahmed <nabeelahmed1699@gmail.com>
2021-09-07 18:37:03 +0500

    first commit to fast food american mockup site

---
+++ script.js
@@ -0,0 +1,183 @@
+< !DOCTYPE html >
+    <
+    html lang = "en" >
+
+    <
+    head >
+    <
+    meta charset = "UTF-8" >
+    <
+    meta http - equiv = "X-UA-Compatible"
+content = "IE=edge" >
+    <
+    meta name = "viewport"
+content = "width=device-width, initial-scale=1.0" >
+    <
+    link rel = "stylesheet"
+href = "main.css" >
+    <
+    title > Burger House < /title> <
+    /head>
+
+<
+body >
+    <
+    header >
+    <
+    nav class = "navbar" >
+    <
+    div class = "logo-container" >
+    <
+    img class = "logo"
+src = "./images/Vector Smart Object.png"
+alt = "" >
+    <
+    h3 class = "logo-text" > Burger House < /h3> <
+    /div> <
+    ul class = "nav-list" >
+    <
+    li > < a class = "nav-link"
+href = "#" > Home < /a></li >
+    <
+    li > < a class = "nav-link"
+href = "#" > Menu < /a></li >
+    <
+    li > < a class = "nav-link"
+href = "#" > Our story < /a></li >
+    <
+    li > < a class = "nav-link"
+href = "#" > contact us < /a></li >
+    <
+    /ul> <
+    /nav> <
+    section class = "section" >
+    <
+    div class = "section-text" >
+    <
+    h3 > < span > Its a good time
+for the great < /span>taste of burgers</h
+3 >
+    <
+    h1 > Burger < /h1> <
+    h2 > Week < /h2> <
+    /div> <
+    div class = "section-imgs" >
+    <
+    a href = "#" > < img class = "header-img"
+src = "./images/Group 1.png"
+alt = "Burger,fries and cold drink png" > < /a> <
+    /div> <
+    /section> <
+    /header>
+
+<!-- ==================== Section Start ======================-->
+<
+section class = "img-section" >
+    <
+    div class = "section-img-container" >
+    <
+    div class = "img-container first" >
+    <
+    img class = "section-img"
+src = "./images/Burger Image.png"
+alt = "Burger-image" >
+    <
+    div class = "text-container" >
+    <
+    p >
+    try it today < /p> <
+    h3 > most popular burger < /h3> <
+    /div> <
+    /div> <
+    div class = "img-container second" >
+    <
+    img class = "section-img"
+src = "./images/Image-1.png"
+alt = "Burger-image" >
+    <
+    div class = "text-container" >
+    <
+    p >
+    try it today < /p> <
+    h3 > more fun more taste < /h3> <
+    /div> <
+    /div> <
+    div class = "img-container third" >
+    <
+    img class = "section-img"
+src = "./images/Image.png"
+alt = "Burger-image" >
+    <
+    div class = "text-container" >
+    <
+    p >
+    try it today < /p> <
+    h3 > fresh and chill < /h3> <
+    /div> <
+    /div> <
+    /div> <
+    /section>
+    <!-- ==================== Section End ========================-->
+
+<!-- Font Awesome icons -->
+<
+script src = "https://kit.fontawesome.com/e0b9e7938f.js"
+crossorigin = "anonymous" > < /script> <
+    script src = "./script.js" > < /script> <
+    /body>
+
+<
+/html>ole.log(2 === '2' || 2 >= 1);
+
+
+// var isSwitch = true;
+// if (false) {
+//     console.log('hey there');
+// } else {
+//     console.log('bandrii');
+// }
+// Multiple if
+// if (2 === 3) {
+//     console.log('first');
+// }
+// if (2 === 2) {
+//     console.log('second');
+// }
+// if (2 === 4) {
+//     console.log('third');
+// } else {
+//     console.log('end');
+// }
+
+// if (2 === 3) {
+//     console.log('first');
+// } else if (2 === 2) {
+//     console.log('second');
+// } else if (2 === 4) {
+//     console.log('third');
+// } else {
+//     console.log('end');
+// }
+// switch (1) {
+//     case 2:
+//         console.log('here');
+//         break;
+//     case 1:
+//         console.log('here');
+//         break;
+//     case 1:
+//         console.log('here');
+//         break;
+
+
+//     case 1:
+//         console.log('here');
+//         break;
+//     case 1:
+//         console.log('here');
+//         break;
+
+//     default:
+//         console.log('in the default');
+//         break;
+// }
